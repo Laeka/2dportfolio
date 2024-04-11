8f62217720e5b6e013f633acde863bcff9e2a3a6
Cristiano <ale.tct16@gmail.com>
2024-04-10 19:08:08 -0500

    initial-commit

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const scaleFactor = 4
+
+export const dialogueData = {
+  pc: `Esta es mi PC. Aquí; trabajo principalmente en JavaScript/Node.js. Además, estoy cursando una beca de Azure Data Science, impulsada por Microsoft, y otra del Ministerio de Trabajo del Perú para Fullstack Developer. De todas formas, puedes encontrar algunos de mis trabajos y, si te gusta, también puedes encontrar mis dotfiles aquí: <a href="https://github.com/laeka" target="_blank">Github</a>!.`,
+  'cs-degree': `Este es mi diploma de egresado emitido por Cibertec en la carrera de Computación e Informática. Este fue uno de los primeros pasos para entrar en el mundo de la programación.`,
+  'sofa-table': `Este es mi sofá. Usualmente está ocupado por mis gatas, pero en este momento deben estar escondidas. Me gusta relajarme aquí viendo documentales y Twitch.`,
+  tv: `Esta es mi televisión. Aquí veo contenido sobre videojuegos como World of Warcraft, Yugioh TCG, pero lo más importante son los youtubers tecnológicos. Recomiendo ver, si deseas hacer este portafolio en 2D, al youtuber que tuvo esta idea: JSlegendDev, además de otro de mis youtubers favoritos, ThePrimeTimeAgen, si te gusta el mundo de Linux y, sobre todo, Neovim.`,
+  bed: `Esta es mi cama. Aquí suelo descansar después de una larga jornada de escribir código, tratar de resolver problemas y quizás un poco de tiempo de juego (gaming).`,
+  resume: `Este es mi escritorio. Aquí podrás encontrar mi currículum:
+  <a href="https://github.com/Laeka/Resume/blob/main/Web%20Developer-Cristian-Tocto-v3-spanish.docx.pdf" target="_blank">Revisa Aqui</a> . También te dejo mi correo electrónico por si estás interesado en contar conmigo en alguna oportunidad de trabajo: ale.tct16@gmail.com.`,
+  projects: `Este portafolio fue creado gracias al tutorial de JsLegendDev. Fue hecho con Kaboom.js, que es una biblioteca para crear juegos con JavaScript. El texto está realizado con HTML/CSS.`,
+  library: `Esta es mi biblioteca. Aquí se encuentran muchos libros, desde historia antigua hasta ciencia ficción actual. Mi favorito es \'Guerra de Sangre\', escrito por Robert Weinberg. Y bueno, ¿quién no es adicto a comprar libros de programación, no importa si no los acabas?`,
+  exit: `Si deseas salir de mi portafolio solo debes cerrar la pestaña.`
+}
